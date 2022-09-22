74803b26f1be71ff1c15880e7f1a1a3c4f11a33b
Lantaio Joy <lantaio@yeah.net>
2022-09-22 19:09:38 +0800

    Create the program file.

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2003-2019 Xwim and/or its affiliates. All rights reserved.
+ * XWIM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ */
+
+/**
+ * 测试中文显示。
+ *
+ * @author Lantaio Joy
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java World!");
+		System.out.println("Java世界，你好！");
+	}
+}
